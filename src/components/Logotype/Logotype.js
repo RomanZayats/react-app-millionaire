37bffdba7f01bf84054b3fb5cb47a8ae2ab22083
Roman Zayats <zayats.roma@gmail.com>
2020-11-15 02:04:00 +0200

    made trivial updates

--- src/components/Logotype/Logotype.js
+++ src/components/Logotype/Logotype.js
@@ -4,7 +4,7 @@ import './Logotype.scss';
 const Logotype = () => {
   return (
     <div className='logotype-container'>
-        <img className='logotype-img' src="./logotype-desktop.png" alt="logotype"/>
+        <img className='logotype-img' src="./gameLogotype.png" alt="logotype"/>
     </div>
   )
 }
